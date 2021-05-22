3d4cb520fd07399ca34d6692d099af74dc7a71e1
Steve Henty <stevehenty@gmail.com>
2021-05-22 08:59:08 +0200

    Support Gravity Forms 2.5

--- button.php
+++ button.php
@@ -54,17 +54,20 @@ function gf_button_shortcode( $shortcode_string, $attributes, $content ){
 	$html = sprintf( '<button id="gf_button_get_form_%d" class="%s">%s</button>', $form_id, $a['button_class'], $a['text'] );
 	$html .= sprintf( '<div id="gf_button_form_container_%d" style="display:none;"></div>', $form_id );
 	$html .= "<script>
-				(function (SHFormLoader, $) {
-				$('#gf_button_get_form_{$form_id}').click(function(){
-					var button = $(this);
-					$.get('{$ajax_url}?action=gf_button_get_form&form_id={$form_id}',function(response){
-						$('#gf_button_form_container_{$form_id}').html(response).fadeIn();
-						button.remove();
-						if(window['gformInitDatepicker']) {gformInitDatepicker();}
-					});
-				});
-			}(window.SHFormLoader = window.SHFormLoader || {}, jQuery));
-			</script>";
+                    (function () {
+                        var initAjaxButton = function() {
+                            jQuery('#gf_button_get_form_{$form_id}').click(function(){
+                                var button = jQuery(this);
+                                jQuery.get('{$ajax_url}?action=gf_button_get_form&form_id={$form_id}',function(response){
+                                    jQuery('#gf_button_form_container_{$form_id}').html(response).fadeIn();
+                                    button.remove();
+                                    if(window['gformInitDatepicker']) {gformInitDatepicker();}
+                                });
+                            });
+                        };
+                        window.addEventListener('DOMContentLoaded', initAjaxButton);
+                    }());
+            </script>";
 	return $html;
 }
 
